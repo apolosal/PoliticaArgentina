61883cba3444c21e19c3751109850459b3730bde
apolosal <apolo1@gmail.com>
2025-11-08 13:54:10 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,9 +6,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import History from "@/pages/History";
 import NotFound from "@/pages/not-found";
-
-// Import PostHog
-import { initPostHog, usePageTracking } from "@/lib/posthog";
 
 function Router() {
   return (
@@ -22,14 +18,6 @@ function Router() {
 }
 
 function App() {
-  // Inicializa PostHog solo en el cliente
-  useEffect(() => {
-    initPostHog();
-  }, []);
-
-  // Trackeo automático de páginas vistas con wouter
-  usePageTracking();
-
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
